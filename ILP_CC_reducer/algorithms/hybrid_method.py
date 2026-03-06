16e4baa7665f02dd53975082e576b5c744557160
anhe2p9 <adriana_nh@uma.es>
2026-03-06 11:19:41 +0100

    Minor changes

--- ILP_CC_reducer/algorithms/hybrid_method.py
+++ ILP_CC_reducer/algorithms/hybrid_method.py
@@ -191,7 +191,7 @@ def hybrid_method_with_full_p_split(model: pyo.AbstractModel, data_dict, objecti
 
             for i,box in enumerate(boxes.copy()):
                 if inside(solution,box):
-                    print(f"        Solution: {solution} inside box: {box}.")
+                    print(f"    Solution: {solution} inside box: {box}.")
                     boxes.pop(i)
                     boxes = full_p_split(box, solution, boxes)
 
